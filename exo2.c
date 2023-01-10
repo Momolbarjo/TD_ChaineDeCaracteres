c4799b22d240367063aad0cd070b28a914e6f2f9
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-01-10 22:35:48 +0100

    Update exo2.c

--- exo2.c
+++ exo2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #define SIZE 1000
 
 
@@ -13,14 +14,40 @@ i = i + 1;
 return (i);
 }
 
+bool areSringEqual (chaine1,chaine2){
+
+if (chaine1==chaine2) {
+
+    return(true);
+}
+else {
+
+    return(false);
+}
+
+}
 
 void main () {
 
-char chaine[SIZE];
+char chaine1[SIZE];
+char chaine2[SIZE];
 
 printf("Ecris ce que tu veux\n");
-scanf("%s",&chaine);
+scanf("%s",&chaine1);
 
- printf("il y a %d caracteres dans la chaine\n", getStringSize(chaine));
+ printf("il y a %d caracteres dans la chaine\n", getStringSize(chaine1));
+
+ printf("Reecris ce que tu veux\n");
+scanf("%s",&chaine2);
+
+if (areSringEqual(chaine1,chaine2)==true){
+
+    printf("Les chaines sont les memes");
+}
+
+else if (areSringEqual(chaine1,chaine2)==false)  {
+
+    printf("Les chaines  ne sont pas les memes");
+}
 
 }
